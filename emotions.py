c677a589637266845db7681dcc0b5b3906b7befa
creative-h <docreativerudra@gmail.com>
2020-02-11 18:45:17 +0530

    function add

--- emotions.py
+++ emotions.py
@@ -8,6 +8,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
+from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plotter
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
@@ -17,41 +18,41 @@ ap.add_argument("--mode",help="train/display")
 a = ap.parse_args()
 mode = a.mode
 
-def plot_model_history(model_history):
-    """
-    Plot Accuracy and Loss curves given the model_history
-    """
-    fig, axs = plt.subplots(1,2,figsize=(15,5))
-    # summarize history for accuracy
-    axs[0].plot(range(1,len(model_history.history['acc'])+1),model_history.history['acc'])
-    axs[0].plot(range(1,len(model_history.history['val_acc'])+1),model_history.history['val_acc'])
-    axs[0].set_title('Model Accuracy')
-    axs[0].set_ylabel('Accuracy')
-    axs[0].set_xlabel('Epoch')
-    axs[0].set_xticks(np.arange(1,len(model_history.history['acc'])+1),len(model_history.history['acc'])/10)
-    axs[0].legend(['train', 'val'], loc='best')
-    # summarize history for loss
-    axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
-    axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
-    axs[1].set_title('Model Loss')
-    axs[1].set_ylabel('Loss')
-    axs[1].set_xlabel('Epoch')
-    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
-    axs[1].legend(['train', 'val'], loc='best')
-    fig.savefig('plot.png')
-    plt.show()
+# def plot_model_history(model_history):
+#     """
+#     Plot Accuracy and Loss curves given the model_history
+#     """
+#     fig, axs = plt.subplots(1,2,figsize=(15,5))
+#     # summarize history for accuracy
+#     axs[0].plot(range(1,len(model_history.history['acc'])+1),model_history.history['acc'])
+#     axs[0].plot(range(1,len(model_history.history['val_acc'])+1),model_history.history['val_acc'])
+#     axs[0].set_title('Model Accuracy')
+#     axs[0].set_ylabel('Accuracy')
+#     axs[0].set_xlabel('Epoch')
+#     axs[0].set_xticks(np.arange(1,len(model_history.history['acc'])+1),len(model_history.history['acc'])/10)
+#     axs[0].legend(['train', 'val'], loc='best')
+#     # summarize history for loss
+#     axs[1].plot(range(1,len(model_history.history['loss'])+1),model_history.history['loss'])
+#     axs[1].plot(range(1,len(model_history.history['val_loss'])+1),model_history.history['val_loss'])
+#     axs[1].set_title('Model Loss')
+#     axs[1].set_ylabel('Loss')
+#     axs[1].set_xlabel('Epoch')
+#     axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+#     axs[1].legend(['train', 'val'], loc='best')
+#     fig.savefig('plot.png')
+#     # plt.show()
 
 # Define data generators
 # train_dir = 'data/train'
 # val_dir = 'data/test'
 
-num_train = 28709
-num_val = 7178
-batch_size = 64
-num_epoch = 50
-
-train_datagen = ImageDataGenerator(rescale=1./255)
-val_datagen = ImageDataGenerator(rescale=1./255)
+# num_train = 28709
+# num_val = 7178
+# batch_size = 64
+# num_epoch = 50
+#
+# train_datagen = ImageDataGenerator(rescale=1./255)
+# val_datagen = ImageDataGenerator(rescale=1./255)
 
 # train_generator = train_datagen.flow_from_directory(
 #         train_dir,
@@ -87,75 +88,84 @@ model.add(Dropout(0.5))
 model.add(Dense(7, activation='softmax'))
 
 # If you want to train the same model or try other models, go for this
-if mode == "train":
-    model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
-
-    model_info = model.fit_generator(
-            train_generator,
-            steps_per_epoch=num_train // batch_size,
-            epochs=num_epoch,
-            validation_data=validation_generator,
-            validation_steps=num_val // batch_size)
-
-    plot_model_history(model_info)
-    model.save_weights('model.h5')
+# if mode == "train":
+#     model.compile(loss='categorical_crossentropy',optimizer=Adam(lr=0.0001, decay=1e-6),metrics=['accuracy'])
+#
+#     model_info = model.fit_generator(
+#             train_generator,
+#             steps_per_epoch=num_train // batch_size,
+#             epochs=num_epoch,
+#             validation_data=validation_generator,
+#             validation_steps=num_val // batch_size)
+#
+#     plot_model_history(model_info)
+#     model.save_weights('model.h5')
 
 # emotions will be displayed on your face from the webcam feed
-elif mode == "display":
-    model.load_weights('model.h5')
+def displayEmoition():
+    if mode == "display":
+        model.load_weights('model.h5')
+        # model = load_model('model.h5')
+        # prevents openCL usage and unnecessary logging messages
+        cv2.ocl.setUseOpenCL(False)
 
-    # prevents openCL usage and unnecessary logging messages
-    cv2.ocl.setUseOpenCL(False)
+        # dictionary which assigns each label an emotion (alphabetical order)
+        emotion_dict = {0: "Angry", 1: "Not-Interested", 2: "Fearful", 3: "Happy", 4: "Confident", 5: "Sad", 6: "Surprised"}
+        net_emotion = {}
 
-    # dictionary which assigns each label an emotion (alphabetical order)
-    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-    net_emotion = {}
+        # start the webcam feed
+        cap = cv2.VideoCapture('output500ms.mp4')
+        while True:
+            # Find haar cascade to draw bounding box around face
+            ret, frame = cap.read()
+            if not ret:
+                break
+            facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
 
-    # start the webcam feed
-    cap = cv2.VideoCapture('output500ms.mp4')
-    while True:
-        # Find haar cascade to draw bounding box around face
-        ret, frame = cap.read()
-        if not ret:
-            break
-        facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = facecasc.detectMultiScale(gray,scaleFactor=1.3, minNeighbors=5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(frame, (x, y-10), (x+w, y+h), (255, 0, 0), 2)
+                roi_gray = gray[y:y + h, x:x + w]
+                cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+                prediction = model.predict(cropped_img)
+                maxindex = int(np.argmax(prediction))
+                cv2.putText(frame, emotion_dict[maxindex], (x+10, y-30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                # print(emotion_dict[maxindex])
+                # print(emotion_dict[maxindex])
+                key = emotion_dict[maxindex]
+                if key in net_emotion:
+                    net_emotion[key] += 1
+                else:
+                    net_emotion.update({key:1})
+            # print(net_emotion)
+            keyMax = max(net_emotion, key = net_emotion.get)
+            all_sum = sum(net_emotion.values())
+            # keyMax = max(net_emotion.items(), key = operator.itemgetter(1))[0]
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y-10), (x+w, y+h), (255, 0, 0), 2)
-            roi_gray = gray[y:y + h, x:x + w]
-            cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-            prediction = model.predict(cropped_img)
-            maxindex = int(np.argmax(prediction))
-            cv2.putText(frame, emotion_dict[maxindex], (x+10, y-30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            # print(emotion_dict[maxindex])
-            # print(emotion_dict[maxindex])
-            key = emotion_dict[maxindex]
-            if key in net_emotion:
-                net_emotion[key] += 1
-            else:
-                net_emotion.update({key:1})
+
+            cv2.imshow('Video', cv2.resize(frame,(160,96),interpolation = cv2.INTER_CUBIC))
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        print("emotion analysis is :", keyMax)
+        # print(all_sum)
         # print(net_emotion)
-        keyMax = max(net_emotion, key = net_emotion.get)
-        all_sum = sum(net_emotion.values())
-        # keyMax = max(net_emotion.items(), key = operator.itemgetter(1))[0]
+        normalized_prediction = {k: (v / float(all_sum)*100)  for k,v in net_emotion.items()}
+        # for key, value in normalized_prediction.items():
+        #     print (key, "{:.2f}".format(value) ,"%")
 
 
-        cv2.imshow('Video', cv2.resize(frame,(160,96),interpolation = cv2.INTER_CUBIC))
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        cap.release()
+        cv2.destroyAllWindows()
+        return normalized_prediction
 
-    print("emotion analysis is :", keyMax)
-    # print(all_sum)
-    # print(net_emotion)
-    normalized_prediction = {k: (v / float(all_sum)*100)  for k,v in net_emotion.items()}
-    for key, value in normalized_prediction.items():
+if __name__== "__main__":
+    x = displayEmoition()
+    for key, value in x.items():
         print (key, "{:.2f}".format(value) ,"%")
 
 
-    cap.release()
-    cv2.destroyAllWindows()
     # pieLabels   = normalized_prediction.keys()
     #
     # populationShare     = normalized_prediction.values()
